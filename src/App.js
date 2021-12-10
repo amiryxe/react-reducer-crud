2bdcc4b611520213efc1e96e57fb754dc20b4848
Amir <nik.amirsa@gmail.com>
2021-12-10 14:13:42 +0330

    install some packages to run mui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div className="App">
-      hello
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
