a3780db6fcada3f581dd5eb7ab54586ee4faf20e
Amir Salehi <nik.amirsa@gmail.com>
2021-12-11 08:59:46 +0330

    notes container

---
+++ src/containers/Notes.js
@@ -0,0 +1,11 @@
+import NoteList from '../components/NoteList';
+import SearchNote from '../components/Notes/Search';
+
+export default function Notes() {
+    return (
+        <>
+            <SearchNote />
+            <NoteList />
+        </>
+    )
+}
