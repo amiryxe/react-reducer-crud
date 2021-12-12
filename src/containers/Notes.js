2887281584db8c3ebec3d691549fb2e15ffdb131
Amir Salehi <nik.amirsa@gmail.com>
2021-12-12 09:10:03 +0330

    create style for notes container and install sass module

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -4,6 +4,7 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
+import './Notes.scss';
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
