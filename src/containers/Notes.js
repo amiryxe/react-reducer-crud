7e2b72ad9ab7587b3bed956e6bb8945f94700f12
Amir Salehi <nik.amirsa@gmail.com>
2021-12-12 09:04:08 +0330

    include grid to project

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,11 +1,36 @@
 import NoteList from '../components/Notes/index';
 import SearchNote from '../components/Notes/Search';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 export default function Notes() {
     return (
         <>
             <SearchNote />
             <NoteList />
+            <Grid container spacing={2}>
+                <Grid item xs={8}>
+                    <Item>xs=8</Item>
+                </Grid>
+                <Grid item xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid item xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid item xs={8}>
+                    <Item>xs=8</Item>
+                </Grid>
+            </Grid>
+
         </>
     )
 }
