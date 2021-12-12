799d69ad173961b3b41859fe76d5bf7ab607800e
Amir Salehi <nik.amirsa@gmail.com>
2021-12-12 09:00:39 +0330

    remove button from notes

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,13 +1,11 @@
 import NoteList from '../components/Notes/index';
 import SearchNote from '../components/Notes/Search';
-import Button from '@mui/material/Button';
 
 export default function Notes() {
     return (
         <>
             <SearchNote />
             <NoteList />
-            <Button variant="contained">Hello World</Button>
         </>
     )
 }
