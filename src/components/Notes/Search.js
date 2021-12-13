d5104329e43a7da06a87e88307e1e41298d7d803
Amir Salehi <nik.amirsa@gmail.com>
2021-12-13 08:39:34 +0330

    seach component

--- src/components/Notes/Search.js
+++ src/components/Notes/Search.js
@@ -1,3 +1,18 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+
 export default function SearchNote() {
-    return 'search note';
+    return (
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                test
+            </CardContent>
+
+            <CardActions>
+                <Button size="small">Search</Button>
+            </CardActions>
+        </Card>
+    )
 }
--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,30 +1,21 @@
 import NoteList from '../components/Notes/index';
 import SearchNote from '../components/Notes/Search';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+import Title from '../components/Notes/Title';
+
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import './Notes.scss';
 
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
-
 export default function Notes() {
     return (
         <Container maxWidth="md">
-            <Grid container spacing={2}>
-                <Grid item xs={8}>
-                    <Item>
-                        <SearchNote />
-                    </Item>
+            <Grid container>
+                <Grid item xs={4}>
+                    <Title />
                 </Grid>
 
-                <Grid item xs={4}>
-                    <Item>title</Item>
+                <Grid item xs={8}>
+                    <SearchNote />
                 </Grid>
 
                 <NoteList />
