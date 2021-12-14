01bd32daf45e1d5a2d4300e1868c07cf121820a5
Amir Salehi <nik.amirsa@gmail.com>
2021-12-14 09:03:01 +0330

    item note structure

--- src/components/Notes/Item.js
+++ src/components/Notes/Item.js
@@ -1,3 +1,29 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 export default function NoteItem() {
-    return 'note item'
+    return (
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    note date is here
+                </Typography>
+                <Typography variant="h5" component="div">
+                    Note title here
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    subtitle or category here
+                </Typography>
+                <Typography variant="body2">
+                    summary of note here
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">View More</Button>
+            </CardActions>
+        </Card>
+    )
 }
