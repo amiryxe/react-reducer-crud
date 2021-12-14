9dab5611279909c9364a4f34a5a34fc7e37baa26
Amir Salehi <nik.amirsa@gmail.com>
2021-12-14 08:58:05 +0330

    change title component

--- src/components/Notes/Title.js
+++ src/components/Notes/Title.js
@@ -1,5 +1,14 @@
+import Typography from '@mui/material/Typography';
+
 export default function Title() {
     return (
-        <h1>React Reducer CRUD</h1>
+        <>
+            <Typography variant="h3" component="h2">
+                React Reducer CRUD
+            </Typography>
+            <Typography variant="subtitle2" gutterBottom component="div">
+                (Note App)
+            </Typography>
+        </>
     )
 }
