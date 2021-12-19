ac5a56cb67211495b5c46c0ba90235e063ef2e5e
Amir Salehi <nik.amirsa@gmail.com>
2021-12-19 09:29:05 +0330

    search changes

--- src/components/Notes/Title.js
+++ src/components/Notes/Title.js
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 export default function Title() {
     return (
         <>
-            <Typography variant="h4" component="h2">
+            <Typography variant="h4" component="h1">
                 React Reducer CRUD
             </Typography>
         </>
