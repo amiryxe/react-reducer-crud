bfbd918344fe379f91bc6b78f5885a0d842cd6b4
Amir <nik.amirsa@gmail.com>
2021-12-24 21:02:58 +0330

    crete basic of reducer

--- src/context/MainState.js
+++ src/context/MainState.js
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import MainContext from "./mainContext";
 
 export default function MainState({ children }) {
     const [state, setState] = useState({
         //...
     });
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "":
+                return {
+                    ...state,
+                    ...action.payload
+                };
+            default:
+                return state;
+        }
+    };
 
     return (
         <MainContext.Provider value={{ state, setState }}>
