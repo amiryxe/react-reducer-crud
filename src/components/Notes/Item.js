5a3e04860e47d1e0b0910ce2fd60e0418f4ee454
Amir Salehi <nik.amirsa@gmail.com>
2022-06-01 07:54:59 +0430

    add alert dialog for delete note

--- src/components/Notes/Item.js
+++ src/components/Notes/Item.js
@@ -5,13 +5,45 @@ import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import NoteDetail from './Detail';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 export default function NoteItem({ data }) {
     const [showModal, setShowModal] = useState(false);
-    const { title, date, description, subtitle } = data;
+    const [showDeleteAlert, setShowDeleteAlert] = useState(false)
+    const { id, title, date, description, subtitle } = data;
+
+    const handleDeleteNote = () => {
+        console.log(id)
+    }
 
     return (
         <Card sx={{ minWidth: 275 }}>
+            <Dialog
+                open={showDeleteAlert}
+                onClose={() => setShowDeleteAlert(false)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Are you sure to delete note?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        You are removing <b>{title}</b>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setShowDeleteAlert(false)}>Cancel</Button>
+                    <Button onClick={handleDeleteNote} autoFocus>
+                        OK
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
             <CardContent>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                     {date}
@@ -28,7 +60,9 @@ export default function NoteItem({ data }) {
             </CardContent>
             <CardActions>
                 <Button size="small" onClick={() => setShowModal(true)}>More</Button>
-                <Button size="small" color="error" onClick={() => setShowModal(true)}>Delete</Button>
+                <Button size="small" color="error" onClick={() => setShowDeleteAlert(true)}>
+                    Delete
+                </Button>
                 <NoteDetail status={showModal} setStatus={setShowModal} />
             </CardActions>
         </Card>
