f2f239a7023a1d8b0592daa35b0a3a6eb8161411
Amir Salehi <nik.amirsa@gmail.com>
2022-08-03 11:19:29 +0430

    create remove dialog component

--- src/components/Notes/Item.js
+++ src/components/Notes/Item.js
@@ -5,49 +5,16 @@ import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import NoteDetail from './Detail';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import MainContext from '../../context/mainContext';
+import RemoveDialog from './RemoveDialog';
 
 export default function NoteItem({ data }) {
     const [showModal, setShowModal] = useState(false);
     const [showDeleteAlert, setShowDeleteAlert] = useState(false)
     const { id, title, date, description, subtitle } = data;
 
-    const { dispatch } = useContext(MainContext)
-
-    const handleDeleteNote = () => {
-        console.log(id)
-        dispatch({ type: 'DELETE_NOTE', payload: id })
-        setShowDeleteAlert(false)
-    }
-
     return (
         <Card sx={{ minWidth: 275 }}>
-            <Dialog
-                open={showDeleteAlert}
-                onClose={() => setShowDeleteAlert(false)}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    Are you sure to delete note?
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        You are removing <b>{title}</b>
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => setShowDeleteAlert(false)}>Cancel</Button>
-                    <Button color="error" onClick={handleDeleteNote} autoFocus>
-                        OK
-                    </Button>
-                </DialogActions>
-            </Dialog>
+            <RemoveDialog />
 
             <CardContent>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
---
+++ src/components/Notes/RemoveDialog.js
@@ -0,0 +1,41 @@
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import MainContext from '../../context/mainContext';
+
+export default function RemoveDialog({ showDeleteAlert, setShowDeleteAlert }) {
+    const { dispatch } = useContext(MainContext)
+
+    const handleDeleteNote = () => {
+        console.log(id)
+        dispatch({ type: 'DELETE_NOTE', payload: id })
+        setShowDeleteAlert(false)
+    }
+
+    return (
+        <Dialog
+            open={showDeleteAlert}
+            onClose={() => setShowDeleteAlert(false)}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+                Are you sure to delete note?
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    You are removing <b>{title}</b>
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => setShowDeleteAlert(false)}>Cancel</Button>
+                <Button color="error" onClick={handleDeleteNote} autoFocus>
+                    OK
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
