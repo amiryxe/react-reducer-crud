b2a70460fac29c52372a812d086e6334c5e0f1b9
Amir Salehi <nik.amirsa@gmail.com>
2022-08-13 09:07:53 +0430

    clean codes

--- src/components/Notes/RemoveDialog.js
+++ src/components/Notes/RemoveDialog.js
@@ -1,11 +1,11 @@
-import { useContext } from 'react';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
-import MainContext from '../../context/mainContext';
+import { useContext } from 'react'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import Button from '@mui/material/Button'
+import MainContext from '../../context/mainContext'
 
 export default function RemoveDialog({ id, title, showDeleteAlert, setShowDeleteAlert }) {
     const { dispatch } = useContext(MainContext)
