b0376c8316238d805379c637345864e1fcf93af4
Amir Salehi <nik.amirsa@gmail.com>
2022-10-01 08:40:57 +0330

    improve context codes

--- src/context/MainState.js
+++ src/context/MainState.js
@@ -1,29 +1,29 @@
-import { useReducer } from "react";
-import MainContext from "./mainContext";
+import { useReducer } from "react"
+import MainContext from "./mainContext"
 
 const saveToLocalStorage = (arr) => {
-    localStorage.setItem('noteList', JSON.stringify(arr));
+    localStorage.setItem('noteList', JSON.stringify(arr))
 }
 
 const initialState = {
     noteList: localStorage.getItem('noteList') ? JSON.parse(localStorage.getItem('noteList')) : []
-};
+}
 
 const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD_NOTE':
-            saveToLocalStorage([...state.noteList, action.payload]);
-            return { noteList: [...state.noteList, action.payload] };
+            saveToLocalStorage([...state.noteList, action.payload])
+            return { noteList: [...state.noteList, action.payload] }
         case 'DELETE_NOTE':
-            saveToLocalStorage(state.noteList.filter(note => note.id !== action.payload));
-            return { noteList: [...state.noteList.filter(item => item.id !== action.payload)] };
+            saveToLocalStorage(state.noteList.filter(note => note.id !== action.payload))
+            return { noteList: [...state.noteList.filter(item => item.id !== action.payload)] }
         default:
-            throw new Error();
+            throw new Error()
     }
 }
 
 export default function MainState({ children }) {
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
         <MainContext.Provider value={{
@@ -32,5 +32,5 @@ export default function MainState({ children }) {
         }}>
             {children}
         </MainContext.Provider>
-    );
+    )
 }
